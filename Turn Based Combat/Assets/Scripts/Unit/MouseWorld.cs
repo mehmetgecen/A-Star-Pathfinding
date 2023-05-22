f6ecb8e2d75a67413002b27e8b1fa272c16d8cba
mehmetgecen <mehmetgecen06@gmail.com>
2023-05-22 20:37:48 +0300

    Update MouseWorld.cs

--- Turn Based Combat/Assets/Scripts/Unit/MouseWorld.cs
+++ Turn Based Combat/Assets/Scripts/Unit/MouseWorld.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
+    private static MouseWorld instance;
+    
     [SerializeField] private LayerMask mouseLayerMask;
-   
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.Log(Physics.Raycast(ray,out RaycastHit hit,float.MaxValue,mouseLayerMask));
-        transform.position = hit.point;
+        Debug.Log(GetPosition());
+        transform.position = GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray,out RaycastHit hit,float.MaxValue,instance.mouseLayerMask);
+        return hit.point;
     }
 }
