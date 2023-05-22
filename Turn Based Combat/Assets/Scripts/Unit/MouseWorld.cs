a333c19776bab93763795883b7c25121a33856a4
mehmetgecen <mehmetgecen06@gmail.com>
2023-05-22 20:29:49 +0300

    Layer Masks
    Cursor now ignores objects except grid plane.

--- Turn Based Combat/Assets/Scripts/Unit/MouseWorld.cs
+++ Turn Based Combat/Assets/Scripts/Unit/MouseWorld.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
+    [SerializeField] private LayerMask mouseLayerMask;
    
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.Log(Physics.Raycast(ray,out RaycastHit hit));
+        Debug.Log(Physics.Raycast(ray,out RaycastHit hit,float.MaxValue,mouseLayerMask));
         transform.position = hit.point;
     }
 }
