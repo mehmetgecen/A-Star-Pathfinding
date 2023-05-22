23683d9ee7f26147f50eef31e3f18389b69eb4d3
mehmetgecen <mehmetgecen06@gmail.com>
2023-05-22 19:39:46 +0300

    Raycasting

---
+++ Turn Based Combat/Assets/Scripts/Unit/MouseWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+   
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.Log(Physics.Raycast(ray));
+    }
+}
